3a7f36ae865f6d87bda63f971ae1100211aacf2c
GLOBAL\i076641 <jingjing.wang01@sap.com>
2018-01-12 16:08:50 +0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,45 @@
 const path = require('path');
 var webpack = require('webpack');
+const HtmlwebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: [
-    "./src/index.js"
-  ],
+  entry: {
+    "huoyun.controls":"./src/index.js"
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
   },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+  },
   module: {
-    rules: [{
-      test: /\.less$/,
-      use: [{
-        loader: "style-loader"
-      }, {
-        loader: "css-loader", options: {
-          sourceMap: true
-        }
-      }, {
-        loader: "less-loader", options: {
-          sourceMap: true
-        }
-      }]
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: /src/,
+        query: {
+          //添加两个presents 使用这两种presets处理js或者jsx文件
+          presets: ['es2015', 'react', 'stage-0']
+        },
+        loader: 'babel-loader'
+      }
     ]
-  }
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Hello World app'
+    })
+  ]
 };
