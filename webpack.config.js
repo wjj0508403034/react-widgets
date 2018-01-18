fd3acaac14103e665d4fae678fbfc6a65ac52ecd
GLOBAL\i076641 <jingjing.wang01@sap.com>
2018-01-18 14:33:20 +0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,23 @@ const path = require('path');
 var webpack = require('webpack');
 const HtmlwebpackPlugin = require('html-webpack-plugin');
 
+const MinifyPlugin = require("babel-minify-webpack-plugin");
+
+const ROOT_PATH = path.resolve(__dirname);
+const TEM_PATH = path.resolve(ROOT_PATH, 'demo');
+
 module.exports = {
   entry: {
-    "huoyun.controls":"./src/index.js"
+    "huoyun.controls": "./src/index.js"
   },
   output: {
-    filename: '[name].js',
+    filename: '[name].js?v=[hash]',
     path: path.resolve(__dirname, 'dist')
   },
   resolve: {
     extensions: ['.js', '.jsx']
   },
+  devtool: 'eval-source-map',
   devServer: {
     historyApiFallback: true,
     hot: true,
@@ -30,16 +36,32 @@ module.exports = {
         exclude: /node_modules/,
         include: /src/,
         query: {
-          //添加两个presents 使用这两种presets处理js或者jsx文件
-          presets: ['es2015', 'react', 'stage-0']
+          presets: ['es2015', 'react', 'stage-0',"minify"]
         },
         loader: 'babel-loader'
       }
     ]
   },
   plugins: [
+    new MinifyPlugin({
+
+    }),
     new HtmlwebpackPlugin({
-      title: 'Hello World app'
+      title: 'HuoYun Widgets Demo Page',
+      template: path.resolve(TEM_PATH, 'index.html'),
+      filename: 'index.html',
+      //chunks这个参数告诉插件要引用entry里面的哪几个入口
+      chunks: ['huoyun.controls'],
+      //要把script插入到标签里
+      inject: 'body',
+      minify: {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+        html5: true,
+        minifyCSS: true,
+        removeComments: true,
+        removeEmptyAttributes: true
+      }
     })
   ]
 };
